a0dd5220818d658f99e1c6eaf000ae9dcc4e2e21
Daniel Leskosky <dleskosky@gmail.com>
2020-07-03 23:12:11 -0400

    Commit for exercise 3.3

--- index.js
+++ index.js
@@ -24,17 +24,28 @@ let persons = [
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<div>Phonebook has info for ${persons.length} people</div>
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people</div>
         <div>${new Date()}</div>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
