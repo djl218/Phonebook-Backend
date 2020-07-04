24df4f9fd6779be67679b51088dfabda0fd17a8c
Daniel Leskosky <dleskosky@gmail.com>
2020-07-03 23:54:40 -0400

    Commit for exercise 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       "name": "Arto Hellas",
@@ -53,7 +55,16 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
-})   
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = Math.random() * 10000
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
