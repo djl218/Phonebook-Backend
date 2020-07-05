e2159053a3597da58433cb811c4faba88de3dba9
Daniel Leskosky <dleskosky@gmail.com>
2020-07-04 22:04:14 -0400

    Commit for exercise 3.9

---
+++ build/precache-manifest.fe2d4e2a22ab32e93accce1dd293f0a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55238aafb60ee4919d6d4eacdc61347b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9981055720eb82f24f14",
+    "url": "/static/js/main.b2fa35b5.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('POSTData', function getPOSTData (request, response) {
     const body = request.body
