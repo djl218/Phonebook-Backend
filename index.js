d47d48ce275d281f0bac64636953226967217cca
Daniel Leskosky <dleskosky@gmail.com>
2020-07-04 21:50:16 -0400

    Commit for exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('POSTData', function getPOSTData (request, response) {
     const body = request.body
@@ -114,7 +116,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })    
