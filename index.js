c9c2e76117065a118e3a8932282f76f09c808f5e
Daniel Leskosky <dleskosky@gmail.com>
2020-07-11 16:32:36 -0400

    Commit for exercises 3.15, 3.16, 3.17, 3.18

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id'})
     }
-
     next(error)
 }
 
