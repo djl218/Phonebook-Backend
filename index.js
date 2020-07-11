585dbbb6fecc205d0a3fa181ba39be7b1aed50c3
Daniel Leskosky <dleskosky@gmail.com>
2020-07-11 18:25:52 -0400

    Commit for exercises 3.19, 3.20, 3.21

--- index.js
+++ index.js
@@ -32,24 +32,20 @@ app.get('/info', (request, response) => {
         <div>${new Date()}</div>`)
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-   
-    if (body.name === undefined) {
-        return response.status(400).json({ error: 'name missing' })
-    }
-    if (body.number === undefined) {
-        return response.status(400).json({ error: 'number missing' })
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson.toJSON())
-    })
+    person.save()
+        .then(savedPerson => savedPerson.toJSON())
+        .then(savedAndFormattedPerson => {
+            response.json(savedAndFormattedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -97,7 +93,10 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
+
     next(error)
 }
 
