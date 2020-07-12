73b13490b923c184d0ab88984967b80317d2937f
Daniel Leskosky <dleskosky@gmail.com>
2020-07-11 20:54:29 -0400

    Commit for exercises 3.19, 3.20, 3.21

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-require('dotenv').config()
 const morgan = require('morgan')
 const Person = require('./models/person')
 
