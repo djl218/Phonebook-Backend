a75cb0b8cf60f5fd25bb024127a1446806d368d2
Daniel Leskosky <dleskosky@gmail.com>
2020-07-11 20:29:17 -0400

    Commit for exercises 3.19, 3.20, 3.21

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
+const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
